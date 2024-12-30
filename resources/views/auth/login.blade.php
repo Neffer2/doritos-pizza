e65fcb9daef9576c91aa7c6528b0b22bfc0bec26
Sebastian <juansecapri@hotmail.com>
2024-12-29 22:07:23 -0500

    Login desktop completo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,45 @@
-<div>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<title>Doritos Pizza | Login</title>
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
-        <!-- Email Address -->
-        <div>
-            <label for="email">Correo</label>
-            <input id="email" type="text" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            @error('email')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+<div class="main-login-container">
+    <div class="login-content">
+        <h1>Iniciar sesión</h1>
+        <form method="POST" class="login-form" action="{{ route('login') }}">
+            @csrf
 
-        <!-- Password -->
-        <div class="mt-4">
-            <label for="password">Contraseña</label>
-            <input id="password" type="password" name="password" :value="old('email')" required autocomplete="current-password" />
-            @error('password')
-                <div>{{ $message }}</div>
-            @enderror
-        </div>
+            <!-- Email Address -->
+            <div class="email-container">
+                <label for="email">Correo electrónico</label>
+                <input id="email" type="text" name="email" :value="old('email')" required autofocus
+                    autocomplete="username" />
+                @error('email')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+            <!-- Password -->
+            <div class="password-container">
+                <label for="password">Contraseña</label>
+                <input id="password" type="password" name="password" :value="old('email')" required
+                    autocomplete="current-password" />
+                @error('password')
+                    <div>{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="forgot-password-container">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
+                        {{ __('¿Olvistaste tu contraseña?') }}
+                    </a>
+                @endif
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+            <div class="login-btn-container">
+                <x-primary-button class="login-btn">
+                    <img src="{{ asset('assets/landing-desktop/aceptar-btn.png') }}" alt="Log in">
+                </x-primary-button>
+            </div>
+        </form>
+    </div>
 </div>
