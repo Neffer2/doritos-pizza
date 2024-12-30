b5faaeeffade05caebc2bda1e58247a03c72c40b
Sebastian <juansecapri@hotmail.com>
2024-12-29 22:42:55 -0500

    Avances de artes en home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.app')
 @section('content')
     <div class="main-home-container">
-        <ul>
-            <li><a href="{{ route('premios') }}">Premios</a></li>
-            <li><a href="{{ route('puntaje') }}">Puntaje</a></li>
-            <li><a href="{{ route('ranking') }}">Ranking</a></li>
-            <li><a href="{{ route('registrar_codigo') }}">Ingresa tu c&oacute;digo</a></li>
-        </ul>
+        <div class="home-info-container">
+            <img src="{{ asset('assets/landing/info-home-text.png') }}" alt="">
+        </div>
+        <div class="home-options-container">
+            <div class="image-row">
+                <a href="{{ route('premios') }}">
+                    <img src="{{ asset('assets/landing/premios-img.png') }}" alt="Premios">
+                </a>
+                <a href="{{ route('puntaje') }}">
+                    <img src="{{ asset('assets/landing/puntaje-img.png') }}" alt="Puntaje">
+                </a>
+            </div>
+            <div class="image-row">
+                <a href="{{ route('ranking') }}">
+                    <img src="{{ asset('assets/landing/ranking-img.png') }}" alt="Ranking">
+                </a>
+                <a href="{{ route('registrar_codigo') }}">
+                    <img src="{{ asset('assets/landing/ingresa-codigo-img.png') }}" alt="Ingresa tu código">
+                </a>
+            </div>
+        </div>
+        <div class="home-info-container">
+            <img src="{{ asset('assets/landing/click-seccion-deseada.png') }}" alt="">
+
+        </div>
     </div>
 @endsection
