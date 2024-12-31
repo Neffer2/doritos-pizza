4e71d96fc2b4e77d4f7a9772a1ec8657dae69c61
Sebastián Camargo <juansecapri@hotmail.com>
2024-12-31 09:47:46 -0500

    Ajustes de plataforma y avances en rebrand

--- resources/views/dashboard/puntaje.blade.php
+++ resources/views/dashboard/puntaje.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Puntaje</h1>
-    puntos: {{ auth()->user()->puntos }}
+    <div class="main-puntaje-container">
+        <div class="volver-logo-container">
+            <a href="/">
+                <img src="{{ asset('assets/landing/volver-logo.png') }}" alt="Volver logo">
+            </a>
+
+        </div>
+        <div class="puntaje-img">
+            <img src="{{ asset('assets/landing/tu-puntaje.png') }}" alt="">
+        </div>
+        <div class="puntaje-text-container">
+            <p class="puntaje-text">Llevas</p>
+            <p class="puntaje-number">
+                {{ auth()->user()->puntos }}
+            </p>
+            <p class="puntaje-info">Puntos</p>
+            <img src="{{ asset('assets/landing/puntaje-info-img.png') }}" alt="">
+        </div>
+
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">	
-        <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-        <title>Doritos Pizza</title>
-        @livewireStyles
-    </head>
-    <body>
-        @yield('content')
-        @livewireScripts
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/puntaje.css') }}?v={{ time() }}">
+    <title>Doritos Pizza</title>
+    @livewireStyles
+</head>
+
+<body>
+    @yield('content')
+    @livewireScripts
+</body>
+
 </html>
