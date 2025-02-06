03dd24dbe898b82f6ba3dc254244b3771f9215b6
Sebastián Camargo <juansecapri@hotmail.com>
2025-02-06 16:00:08 -0500

    Ultima versión

--- resources/views/livewire/registro-codigos.blade.php
+++ resources/views/livewire/registro-codigos.blade.php
@@ -9,11 +9,11 @@
         <img class="doritos-logo-codigos" src=" {{ asset('assets/landing/doritos-logo.png') }}" alt="">
         <img class="info-text-1" src="{{ asset('assets/landing/codigo-text-info1.png') }}" alt="">
         <input class="input-codigo" type="text" wire:model.lazy="codigo">
-        <button class="btn-codigo" wire:click="RegistrarCodigo">Registrar</button>
         @session('error')
             <span class="error-codigo-text">{{ session('error') }}</span>
         @endsession
         <img class="info-text-2" src="{{ asset('assets/landing/codigo-text-info.png') }}" alt="">
+        <button class="btn-codigo" wire:click="RegistrarCodigo">Registrar</button>
     </div>
 
     <div class="codigo-img-container">
